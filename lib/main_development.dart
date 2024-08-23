9938e0768eb1f5b910b8093da0893803c3c42b07
NonymousMorlock <agamspaul@gmail.com>
2024-08-23 21:12:18 +0000

    ✅ Build: Wrap the App with DevicePreview in the development environment

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,7 @@
 import 'package:cardano_wallet_reader/app/app.dart';
 import 'package:cardano_wallet_reader/bootstrap.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  bootstrap(() => const App());
+  bootstrap(() => DevicePreview(builder: (_) => const App()));
 }
