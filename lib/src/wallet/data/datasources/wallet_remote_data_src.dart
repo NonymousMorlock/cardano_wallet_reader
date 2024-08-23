63681f9475a0ea4bce1eaeddb0ab3ad33040670e
NonymousMorlock <agamspaul@gmail.com>
2024-08-23 23:25:02 +0000

    ✨ Feat: Create Wallet remote data source implementation stub

--- lib/src/wallet/data/datasources/wallet_remote_data_src.dart
+++ lib/src/wallet/data/datasources/wallet_remote_data_src.dart
@@ -1,6 +1,7 @@
 import 'package:cardano_wallet_reader/core/enums/order.dart';
 import 'package:cardano_wallet_reader/src/wallet/data/models/transaction_model.dart';
 import 'package:cardano_wallet_reader/src/wallet/data/models/wallet_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract interface class WalletRemoteDataSrc {
   Future<List<TransactionModel>> getTransactions({
@@ -11,4 +12,27 @@ abstract interface class WalletRemoteDataSrc {
   });
 
   Future<WalletModel> getWallet(String walletAddress);
+}
+
+class WalletRemoteDataSrcImpl implements WalletRemoteDataSrc {
+  const WalletRemoteDataSrcImpl(this._client);
+
+  final http.Client _client;
+
+  @override
+  Future<List<TransactionModel>> getTransactions({
+    required String walletAddress,
+    SortOrder? order,
+    int? limit,
+    int? page,
+  }) async {
+    // TODO: implement getTransactions
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<WalletModel> getWallet(String walletAddress) async {
+    // TODO: implement getWallet
+    throw UnimplementedError();
+  }
 }
