aae7199f90c37d38b45353fb726b36ca9418173f
NonymousMorlock <agamspaul@gmail.com>
2024-08-23 22:59:48 +0000

    ✨ Feat: Implement wallet repository implementation

--- lib/src/wallet/data/repos/wallet_repo_impl.dart
+++ lib/src/wallet/data/repos/wallet_repo_impl.dart
@@ -1,9 +1,12 @@
 import 'package:cardano_wallet_reader/core/enums/order.dart';
+import 'package:cardano_wallet_reader/core/errors/exception.dart';
+import 'package:cardano_wallet_reader/core/errors/failure.dart';
 import 'package:cardano_wallet_reader/core/utils/typedefs.dart';
 import 'package:cardano_wallet_reader/src/wallet/data/datasources/wallet_remote_data_src.dart';
 import 'package:cardano_wallet_reader/src/wallet/domain/entities/transaction.dart';
 import 'package:cardano_wallet_reader/src/wallet/domain/entities/wallet.dart';
 import 'package:cardano_wallet_reader/src/wallet/domain/repos/wallet_repo.dart';
+import 'package:dartz/dartz.dart';
 
 class WalletRepoImpl implements WalletRepo {
   const WalletRepoImpl(this._remoteDataSrc);
@@ -17,13 +20,27 @@ class WalletRepoImpl implements WalletRepo {
     int? limit,
     int? page,
   }) async {
-    // TODO: implement getTransactions
-    throw UnimplementedError();
+    try {
+      final result = await _remoteDataSrc.getTransactions(
+        walletAddress: walletAddress,
+        order: order,
+        limit: limit,
+        page: page,
+      );
+
+      return Right(result);
+    } on ServerException catch (e) {
+      return Left(ServerFailure.fromServerException(e));
+    }
   }
 
   @override
   ResultFuture<Wallet> getWallet(String walletAddress) async {
-    // TODO: implement getWallet
-    throw UnimplementedError();
+    try {
+      final result = await _remoteDataSrc.getWallet(walletAddress);
+      return Right(result);
+    } on ServerException catch (e) {
+      return Left(ServerFailure.fromServerException(e));
+    }
   }
 }
