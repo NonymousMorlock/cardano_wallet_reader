79f8b2fac4c9efbff725ace38858edbf85b27dd4
NonymousMorlock <agamspaul@gmail.com>
2024-08-25 02:23:04 +0000

    ✨ Feat: Create CoreUtils for the re-usable utility functions

---
+++ lib/core/utils/core_utils.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class CoreUtils {
+  static void showSnackBar(
+    BuildContext context, {
+    required String message,
+    Color? colour,
+    Color? textColour,
+  }) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          message,
+          style: GoogleFonts.roboto(
+            color: textColour ?? Colors.white,
+            fontSize: 16,
+          ),
+        ),
+        backgroundColor: colour ?? Colors.red,
+        behavior: SnackBarBehavior.floating,
+      ),
+    );
+  }
+
+  static Future<void> copyToClipboard(
+    BuildContext context, {
+    required String text,
+  }) async {
+    await Clipboard.setData(ClipboardData(text: text));
+    if (context.mounted) {
+      showSnackBar(
+        context,
+        message: 'Copied to clipboard',
+        colour: Colors.green,
+      );
+    }
+  }
+}
