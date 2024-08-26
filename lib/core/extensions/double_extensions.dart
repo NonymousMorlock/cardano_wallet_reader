edecba82fbf6d4ae2e88500ced6f8d2aea39bdf9
NonymousMorlock <agamspaul@gmail.com>
2024-08-26 02:05:06 +0000

    🧹 Refactor: Change the logic for formatting doubles

--- lib/core/extensions/double_extensions.dart
+++ lib/core/extensions/double_extensions.dart
@@ -1,9 +1,10 @@
+import 'package:intl/intl.dart';
 extension DoubleExtensions on double {
   String get walletFormat {
     final stripped = this % 1 == 0 ? toInt() : this;
-    return stripped.toString().replaceAllMapped(
-          RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
-          (match) => '${match[1]},',
-        );
+    return NumberFormat(
+      '###,###.##',
+      'en_US',
+    ).format(stripped);
   }
 }
