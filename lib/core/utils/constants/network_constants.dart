f183c25f82e5587497b466117881d26e8d859269
NonymousMorlock <agamspaul@gmail.com>
2024-08-26 02:04:26 +0000

    ✨ Feat: Add authority and apiEndpoint to NetworkConstants

--- lib/core/utils/constants/network_constants.dart
+++ lib/core/utils/constants/network_constants.dart
@@ -1,4 +1,6 @@
 abstract class NetworkConstants {
   static const apiKey = String.fromEnvironment('BLOCKFROST_TOKEN');
   static const baseUrl = 'https://cardano-preview.blockfrost.io/api/v0';
+  static const authority = 'cardano-preview.blockfrost.io';
+  static const apiEndpoint = '/api/v0';
 }
