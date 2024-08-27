7cfdfbe20220332e2d4dc8fbe41e2aecfdc87451
NonymousMorlock <agamspaul@gmail.com>
2024-08-27 23:51:47 +0000

    ❌ Breaking Change: Change DevicePreview config to only show the DevicePreview on Desktop devices

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,7 +1,14 @@
+import 'dart:io';
+
 import 'package:cardano_wallet_reader/app/app.dart';
 import 'package:cardano_wallet_reader/bootstrap.dart';
 import 'package:device_preview/device_preview.dart';
 
 void main() {
-  bootstrap(() => DevicePreview(builder: (_) => const App()));
+  bootstrap(
+    () => DevicePreview(
+      enabled: Platform.isWindows || Platform.isLinux || Platform.isMacOS,
+      builder: (_) => const App(),
+    ),
+  );
 }
