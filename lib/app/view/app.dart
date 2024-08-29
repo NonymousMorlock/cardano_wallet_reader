27cb235ca7c580a6b622838ad93472d70ca03ade
NonymousMorlock <agamspaul@gmail.com>
2024-08-29 18:01:05 +0000

    ✨ Feat: Add the HomeView as the `home` of the App

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,7 +1,10 @@
-import 'package:cardano_wallet_reader/counter/counter.dart';
+import 'package:cardano_wallet_reader/core/services/injection_container.dart';
 import 'package:cardano_wallet_reader/l10n/l10n.dart';
+import 'package:cardano_wallet_reader/src/home/presentation/views/home_view.dart';
+import 'package:cardano_wallet_reader/src/wallet/presentation/wallet_views.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -27,7 +30,10 @@ class App extends StatelessWidget {
       supportedLocales: AppLocalizations.supportedLocales,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
-      home: const CounterPage(),
+      home: BlocProvider(
+        create: (_) => sl<WalletCubit>(),
+        child: const HomeView(),
+      ),
     );
   }
 }
